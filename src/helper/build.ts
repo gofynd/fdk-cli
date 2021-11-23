040b1bb94007364fc640c4645531e23e876cbe8f
Akash Mane <akashmane@gofynd.com>
2021-11-23 13:17:02 +0530

    fix

--- src/helper/build.ts
+++ src/helper/build.ts
@@ -1,18 +1,20 @@
-import { exec } from 'child_process'
+import { exec } from 'child_process';
 import { createDirectory } from './file.utils';
 
 export function build({ buildFolder, imageCdnUrl, assetCdnUrl }) {
     return new Promise((resolve, reject) => {
-        let b = exec(`node ./node_modules/@vue/cli/bin/vue.js build --target lib --dest ${buildFolder} --name themeBundle theme/index.js`,
+        let b = exec(
+            `node ./node_modules/@vue/cli/bin/vue.js build --target lib --dest ${buildFolder} --name themeBundle theme/index.js`,
             {
                 cwd: process.cwd(),
                 env: {
                     ...process.env,
                     IMAGE_CDN_URL: imageCdnUrl,
                     ASSET_CDN_URL: assetCdnUrl,
-                    NODE_ENV: "production"
-                }
-            });
+                    NODE_ENV: 'production',
+                },
+            }
+        );
 
         b.stdout.pipe(process.stdout);
         b.stderr.pipe(process.stderr);
@@ -26,31 +28,38 @@ export function build({ buildFolder, imageCdnUrl, assetCdnUrl }) {
     });
 }
 interface DevBuild {
-    buildFolder: string,
-    imageCdnUrl: string,
-    isProd: boolean
+    buildFolder: string;
+    imageCdnUrl: string;
+    isProd: boolean;
 }
-export function devBuild({ buildFolder, imageCdnUrl, isProd } : DevBuild) {
+export function devBuild({ buildFolder, imageCdnUrl, isProd }: DevBuild) {
     return new Promise((resolve, reject) => {
-        createDirectory(buildFolder);
-        let b = exec(`node ./node_modules/@vue/cli/bin/vue.js build --target lib --dest ${buildFolder} --name themeBundle theme/index.js`,
-            {
-                cwd: process.cwd(),
-                env: {
-                    ...process.env,
-                    IMAGE_CDN_URL: imageCdnUrl,
-                    NODE_ENV: (isProd && "production") || "development"
+        try {
+            createDirectory(buildFolder);
+            let b = exec(
+                `node ./node_modules/@vue/cli/bin/vue.js build --target lib --dest ${buildFolder} --name themeBundle theme/index.js`,
+                {
+                    cwd: process.cwd(),
+                    env: {
+                        ...process.env,
+                        IMAGE_CDN_URL: imageCdnUrl,
+                        NODE_ENV: (isProd && 'production') || 'development',
+                    },
                 }
+            );
+
+            b.stdout.pipe(process.stdout);
+            b.stderr.pipe(process.stderr);
+
+            b.on('exit', function (code) {
+                if (!code) {
+                    return resolve(true);
+                }
+                reject({ message: 'Build Failed' });
             });
-
-        b.stdout.pipe(process.stdout);
-        b.stderr.pipe(process.stderr);
-
-        b.on('exit', function (code) {
-            if (!code) {
-                return resolve(true);
-            }
+        } catch (error) {
+            console.log(error);
             reject({ message: 'Build Failed' });
-        });
+        }
     });
 }
