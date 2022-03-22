3fe793c18b5a4b7b05117174663ad180283b9af5
aayush jain <aayushvjain@gofynd.com>
2022-03-22 19:03:52 +0530

    update extension example repo url

--- src/partners/cmds/extension_cmds/init.js
+++ src/partners/cmds/extension_cmds/init.js
@@ -35,7 +35,7 @@ function validateEmpty(input) {
 }
 
 // TODO: add public repo name from github
-const INIT_PROJECT_URL = "git@github.com:gofynd/example-extension-javascript.git";//"https://gitlab.com/fynd/regrowth/fynd-platform/extensions/groot.git";
+const INIT_PROJECT_URL = "https://github.com/gofynd/example-extension-javascript.git";//"https://gitlab.com/fynd/regrowth/fynd-platform/extensions/groot.git";
 
 const questions = [
     {
