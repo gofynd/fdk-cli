7f62d5057ca58892c02879db05baeef427668d53
Shivraj Koli <shivrajkoli@gofynd.com>
2022-04-18 17:34:13 +0530

    Added review changes

---
+++ src/helper/constants.ts
@@ -0,0 +1,4 @@
+export const ALL_THEME_COMMANDS = ['new', 'init', 'sync', 'serve', 'publish', 'unpublish', 'pull', 'pull-config', 'context', 'context-list']
+export const THEME_COMMANDS = ['serve', 'sync', 'new', 'init'];
+export const ENVIRONMENT_COMMANDS = ['env'];
+export const AUTHENTICATION_COMMANDS = ['login', 'logout', 'auth'];
