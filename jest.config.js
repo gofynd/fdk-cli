0732a67a02b610696de8487a2e32d822d8a2cab4
Anurag Pandey <anuragpandey@gofynd.com>
2022-04-19 16:10:42 +0530

    setup for testcases

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/__tests__'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
