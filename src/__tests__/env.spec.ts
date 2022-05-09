aadf6ad9c03b979c1b5c754e3dbfa4a06f722d22
Anurag Pandey <anuragpandey@gofynd.com>
2022-05-09 15:00:36 +0530

    env test modified

--- src/__tests__/env.spec.ts
+++ src/__tests__/env.spec.ts
@@ -4,25 +4,30 @@ import configStore, { CONFIG_KEYS } from '../lib/Config';
 let program;
 
 describe('Env Commands', () => {
+    beforeEach( () => {
+        // program.commands.forEach(command => {
+        //     command._optionValues = {};
+        // });
+    });
     beforeAll(async () => {
         program = await init('fdk');
     });
     it('should console current set env', async () => {
-        await program.parseAsync(['node', './bin/fdk.js', 'env', 'set', '-n', 'fyndx0']);
+        await program.parseAsync(['ts-node', './src/fdk.ts', 'env', 'set', '-n', 'fyndx0']);
         const currentEnv = configStore.get(CONFIG_KEYS.CURRENT_ENV_VALUE);
         expect(currentEnv).toMatch('fyndx0');
     });
 
     it('should console current active env', async () => {
-        await program.parseAsync(['node', './bin/fdk.js', 'env', 'set', '-n', 'fyndx0']);
-        await program.parseAsync(['node', './bin/fdk.js', 'env', 'get']);
+        await program.parseAsync(['ts-node', './src/fdk.ts', 'env', 'set', '-n', 'fyndx0']);
+        await program.parseAsync(['ts-node', './src/fdk.ts', 'env', 'get']);
         const currentEnv = configStore.get(CONFIG_KEYS.CURRENT_ENV_VALUE);
         expect(currentEnv).toMatch('fyndx0');
     });
 
     it('should console all env list', async () => {
-        await program.parseAsync(['node', './bin/fdk.js', 'env', 'set', '-n', 'fyndx0']);
-        await program.parseAsync(['node', './bin/fdk.js', 'env', 'ls']);
+        await program.parseAsync(['ts-node', './src/fdk.ts', 'env', 'set', '-n', 'fyndx0']);
+        await program.parseAsync(['ts-node', './src/fdk.ts', 'env', 'ls']);
         const currentEnv = configStore.get(CONFIG_KEYS.CURRENT_ENV_VALUE);
         expect(currentEnv).toMatch('fyndx0');
     });
