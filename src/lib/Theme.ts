a89f7c39ceda11e6b16b17d7b2a004a31cd30dd6
Anurag Pandey <anuragpandey@gofynd.com>
2022-06-29 11:55:44 +0530

    merge branch changes

--- src/lib/Theme.ts
+++ src/lib/Theme.ts
@@ -48,7 +48,7 @@ export default class Theme {
         pull-config
     */
     static TEMPLATE_DIRECTORY = path.join(__dirname, '../../template');
-    static BUILD_FOLDER = './dist';
+    static BUILD_FOLDER = './.fdk/dist';
     static SRC_FOLDER = './.fdk/temp-theme';
     static SRC_ARCHIVE_FOLDER = './.fdk/archive';
     static ZIP_FILE_NAME = `archive.zip`;
@@ -265,6 +265,7 @@ export default class Theme {
             await build({ buildFolder: Theme.BUILD_FOLDER, imageCdnUrl, assetCdnUrl });
 console.log("build dones")
             // check if build folder exists, as during build, vue fails with non-error code even when it errors out
+            console.log("paths",Theme.BUILD_FOLDER)
             if (!fs.existsSync(Theme.BUILD_FOLDER)) {
                 throw new Error('Build Failed');
             }
