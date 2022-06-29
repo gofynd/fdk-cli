79fac1bf512e2a393db89df90e880b9bf8143da1
Vivek Prajapati <vivekprajapati@gofynd.com>
2022-06-29 16:20:56 +0530

    changes

--- src/lib/Theme.ts
+++ src/lib/Theme.ts
@@ -736,8 +736,8 @@ export default class Theme {
     };
     private static copyThemeSourceToFdkFolder = async () => {
         try {
-            await fs.copy(path.normalize('./theme'), Theme.SRC_FOLDER);
-            fs.copyFileSync(path.normalize('./package.json'), path.normalize(Theme.SRC_FOLDER + '/package.json'));
+            await fs.copy(path.join(process.cwd(),'./theme'), Theme.SRC_FOLDER);
+            fs.copyFileSync(path.join('./package.json'), path.normalize(Theme.SRC_FOLDER + '/package.json'));
             await archiveFolder({
                 srcFolder: Theme.SRC_FOLDER,
                 destFolder: Theme.SRC_ARCHIVE_FOLDER,
