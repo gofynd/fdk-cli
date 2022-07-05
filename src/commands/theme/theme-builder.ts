60cb3ee4459839287aba3c5b25a77314897d2415
Anurag Pandey <anuragpandey@gofynd.com>
2022-07-05 15:32:21 +0530

    active_context command created

--- src/commands/theme/theme-builder.ts
+++ src/commands/theme/theme-builder.ts
@@ -46,5 +46,6 @@ export default function themeCommandBuilder() {
 
     theme.command('context-list').description('List all contexts').asyncAction(ThemeContext.listThemeContext);
 
+    theme.command('active-context').description('print active_context').asyncAction(ThemeContext.activeContext);
     return theme;
 }
