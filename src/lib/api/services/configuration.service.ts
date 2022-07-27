5739d54a8bb6a68c6df96f3e6d70f90f2c4aa44c
Shivraj Koli <kolishivraj05@gmail.com>
2022-07-28 01:42:03 +0530

    [Config service] replace cmdErr with consolidateError

--- src/lib/api/services/configuration.service.ts
+++ src/lib/api/services/configuration.service.ts
@@ -1,5 +1,5 @@
 import { getActiveContext } from '../../../helper/utils';
-import CommandError from '../../CommandError';
+import { consolidateErrorMessage } from '../../../helper/error.utils';
 import ApiClient from '../ApiClient';
 import { URLS } from './url';
 import { getCommonHeaderOptions } from './utils';
@@ -9,13 +9,14 @@ export default {
     try {
       const activeContext = data ?  data : getActiveContext();
       const axiosOption = Object.assign({}, getCommonHeaderOptions());
-      return ApiClient.get(
+      const res = await ApiClient.get(
         URLS.GET_APPLICATION_DETAILS(activeContext.application_id, activeContext.company_id),
         axiosOption
       );
-      // return SDK.configuration
+      return res;
     } catch (error) {
-      throw new CommandError(error.message, error.code);
+      consolidateErrorMessage(error?.response?.status, error?.response?.statusText, error?.response?.data?.message, error?.request?.path)
+      throw new Error();
     }
   },
 };
