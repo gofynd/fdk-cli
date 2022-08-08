8fd52321b6666f51344c778aa2d7f713aa107915
Anurag Pandey <anuragpandey1115@gmail.com>
2022-08-08 14:21:39 +0530

    msg changes

--- src/lib/ThemeContext.ts
+++ src/lib/ThemeContext.ts
@@ -47,7 +47,7 @@ export default class ThemeContext {
             }
             if (!hasContext()) {
                 Logger.warn('No theme contexts available');
-                Logger.success('Add a theme context using `fdk theme context -t [your-theme-token] -n [context-name]`');
+                Logger.success('Add a theme context using `fdk theme context -t [your-theme-token] -n [context-name]` ');
                 return;
             }
             const contextPath = path.join(process.cwd(), '.fdk','context.json');
