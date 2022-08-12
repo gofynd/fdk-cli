b7c0cec22799cc17c7b22efdf198a85e4e708922
Shivraj Koli <54741030+Shivraj0@users.noreply.github.com>
2022-08-12 16:14:14 +0530

    Feature consolidate api error messages (#102)
    * Added common consolidated err message func
    
    * [Auth service] replace cmdErr with consolidateError
    
    * [Config service] replace cmdErr with consolidateError
    
    * [Theme service] replace cmdErr with consolidateError
    
    * [Upload service] replace cmdErr with consolidateError
    
    * [company service] replace cmdErr with consolidateError
    
    * Removed redundant new Error instance
    
    * Added error method in interceptor + added `method` key in error log

--- src/lib/api/services/company_setup.service.ts
+++ src/lib/api/services/company_setup.service.ts
@@ -1,20 +1,26 @@
 import ApiClient from "../ApiClient";
 import { URLS } from './url'
 import { getCommonHeaderOptions } from "./utils";
+import { consolidateErrorMessage } from "../../../helper/error.utils";
 
 export default {
-    setupCompany: (company_id, request_id, data = {}) => {
-        const axiosOption = Object.assign(
-            {
-                params: {
-                    request_id
-                }
-            },
-            {
-                data: data
-            },
-            getCommonHeaderOptions()
-        );
-        return ApiClient.post(URLS.SETUP_COMPANY(company_id), axiosOption);
+    setupCompany: async (company_id, request_id, data = {}) => {
+        try {
+            const axiosOption = Object.assign(
+                {
+                    params: {
+                        request_id
+                    }
+                },
+                {
+                    data: data
+                },
+                getCommonHeaderOptions()
+            );
+            const res = await ApiClient.post(URLS.SETUP_COMPANY(company_id), axiosOption);
+            return res;   
+        } catch(error) {
+            consolidateErrorMessage(error?.response?.status, error?.response?.statusText, error?.request?.method, error?.response?.data?.message, error?.request?.path);
+        }
     },
 }
