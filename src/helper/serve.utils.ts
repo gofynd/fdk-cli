3cf19942c07bebd8f0fa1573aa613a2ea753fece
anuragpandey1115 <anuragpandey1115@gmail.com>
2022-12-29 17:52:42 +0530

    loader added

--- src/helper/serve.utils.ts
+++ src/helper/serve.utils.ts
@@ -119,7 +119,8 @@ export async function startServer({ domain, host, isSSR, port }) {
 		return res.end()
 	})
 	app.get('/*', async (req, res) => {
-
+		const BUNDLE_PATH = path.join(process.cwd(), path.join('.fdk', 'dist', 'themeBundle.common.js'));
+		if(!fs.existsSync(BUNDLE_PATH)) return res.sendFile(path.join(__dirname,'../../','/src/helper','/loader.html'));
 		if (req.originalUrl == '/favicon.ico' || req.originalUrl == '/.webp') {
 			return res.status(404).send('Not found');
 		}
