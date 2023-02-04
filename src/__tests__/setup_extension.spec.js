cfcd4a5c63440b7ab37c25d3aca8e81b041e2c5e
Vivek Prajapati <vivekprajapati@gofynd.com>
2023-02-04 11:11:38 +0530

    test cases fixes

--- src/__tests__/setup_extension.spec.js
+++ src/__tests__/setup_extension.spec.js
@@ -7,14 +7,6 @@ const fs = require('fs');
 const { NODE_VUE } = require('../partners/utils/extension-constant');
 let extensionUtils = require('../partners/utils/extension-utils');
 
-function timeOutHelper(seconds) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(1);
-        }, seconds * 1000);
-    });
-}
-
 
 describe('Setup Extension Command', () => {
     afterEach(async () => {
@@ -37,10 +29,7 @@ describe('Setup Extension Command', () => {
     
     it('should clone template files', async () => {
         const envFileData = `EXTENSION_API_KEY="api_key"\nEXTENSION_API_SECRET="api_secret"\nEXTENSION_BASE_URL="https://abc.com"\nEXTENSION_CLUSTER_URL="https://api.fynd.com"`;
-
-        yargs('extension setup').wrap(null).commandDir('../partners/cmds').parse();
-        await timeOutHelper(10);
-        
+        await yargs().wrap(null).commandDir('../partners/cmds').parseAsync('extension setup');
         expect(fs.existsSync('./Test_Extension')).toEqual(true);
         expect(fs.existsSync('./Test_Extension/.fdk')).toEqual(true)
         expect(fs.readFileSync('./Test_Extension/.env',  { encoding: 'utf-8' })).toBe(envFileData);
@@ -49,14 +38,13 @@ describe('Setup Extension Command', () => {
     });
     
     it('should throw directory already exists error', async () => {
-        jest.spyOn(process, 'exit').mockImplementation(() => {
-            throw new Error("Directory already exists")
-        })
-
-        yargs('extension setup --target-dir ./bin').wrap(null).commandDir('../partners/cmds').parse();
-        await timeOutHelper(3);
-
-        expect(fs.existsSync('./Test_Extension')).toEqual(false);
-        expect(fs.existsSync('./bin/package.json')).toEqual(false);
+        try{
+            jest.spyOn(process, 'exit').mockImplementation(() => {
+                throw new Error("Directory already exists");
+            })
+            await yargs().wrap(null).commandDir('../partners/cmds').parseAsync('extension setup --target-dir ./bin');
+        }catch(err){
+            expect(err.message).toBe('Directory already exists');
+        }
     })
 });
