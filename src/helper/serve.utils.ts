df2b8c0258edb228e3ef980ff332d6d918db517f
Anurag Pandey <64393814+anuragpandey1115@users.noreply.github.com>
2023-02-21 16:13:42 +0530

    ID:FPCO-7517; DONE: 100; HOURS: 24; FDK theme serve is breaking when … (#175)
    * ID: FPCO-7517; DONE: 100; HOURS: 24; FDK theme serve is breaking when an error occurs while proxying a request from fdk
    
    ---------
    
    Co-authored-by: anuragpandey1115 <anuragpandey1115@gmail.com>

--- src/helper/serve.utils.ts
+++ src/helper/serve.utils.ts
@@ -88,7 +88,8 @@ export async function startServer({ domain, host, isSSR, port }) {
 		target: currentDomain, // target host
 		changeOrigin: true, // needed for virtual hosted sites
 		cookieDomainRewrite: 'localhost', // rewrite cookies to localhost
-		onProxyReq: fixRequestBody
+		onProxyReq: fixRequestBody,
+		onError: error => Logger.error(error)
 	  };
 
 	  // proxy to solve CORS issue
