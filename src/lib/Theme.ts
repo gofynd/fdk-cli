9b9226a2a833babdc9a73183db4e47c6eafcb695
Manish Prajapati <manishprajapati@gofynd.com>
2023-06-28 15:47:55 +0530

    ID:FPCO-17763;HOURS:2;DONE:100; Check whether available_section in the db is available in local code or not

--- src/lib/Theme.ts
+++ src/lib/Theme.ts
@@ -407,8 +407,38 @@ export default class Theme {
                 throw new CommandError(`Invalid config.json`);
             }
 
+            const available_sections_in_db = theme.available_sections
             let available_sections = await Theme.getAvailableSectionsForSync();
             await Theme.validateAvailableSections(available_sections);
+
+            // validate available section
+            const db_section_names = available_sections_in_db.map(section => section.name)
+            const local_section_names = available_sections.map(section => section.name)
+            const mismatched_sections = _.difference(db_section_names, local_section_names)
+
+            if(mismatched_sections.length > 0){
+                const questions = [
+                    {
+                        type: 'confirm',
+                        name: 'proceed',
+                        message: `Certain theme sections are unavailable in your code. Proceeding with the sync may result in potential side effects and issues. Would you like to continue?`,
+                    },
+                ];
+
+                const log_section_details = mismatched_sections.map(name => `❌ ${name} section is not present in your code`).join("\n")
+
+                // Show which section is not present and in which page it is set as an preset section.
+                // Ex. ❌ hero_image section used in home page is not available
+                console.log('\n' + chalk.yellow(log_section_details) + '\n');
+
+                await inquirer.prompt(questions).then(async answers => {
+                    if (answers.proceed) {
+                        Logger.info('Proceeding without unavailable sections...');
+                    } else {
+                        throw new Error(`Please review the sections. Some of the sections that were available in the theme are missing from your code.`)
+                    }
+                });
+            }
             
             // Create index.js with section file imports
             await Theme.createSectionsIndexFile(available_sections);
