512eaa9f6fa4e6b30c9a3ec3420e8ca6bfa2c957
Manish Prajapati <manishprajapati@gofynd.com>
2023-07-19 20:43:36 +0530

    Remove npmrc and add NODE_OPTION with version check

--- src/helper/build.ts
+++ src/helper/build.ts
@@ -9,6 +9,7 @@ export function build({ buildFolder, imageCdnUrl, assetCdnUrl, assetHash = '' })
     const spinner = new Spinner('Building assets using vue-cli-service');
     return new Promise((resolve, reject) => {
         spinner.start();
+        const isNodeVersionIsGreaterThan18 = +process.version.split(".")[0].slice(1) >= 18;
         let b = exec(`node ${VUE_CLI_PATH} build --target lib --dest ${buildFolder} --name themeBundle --filename ${assetHash}_themeBundle ${THEME_ENTRY_FILE}`,
             {
                 cwd: process.cwd(),
@@ -19,6 +20,7 @@ export function build({ buildFolder, imageCdnUrl, assetCdnUrl, assetHash = '' })
                     ASSET_HASH: assetHash,
                     NODE_ENV: "production",
                     VUE_CLI_SERVICE_CONFIG_PATH: path.join(process.cwd(), Theme.VUE_CLI_CONFIG_PATH),
+                    ...(isNodeVersionIsGreaterThan18 && { NODE_OPTIONS:"--openssl-legacy-provider" })
                 }
             });
 
@@ -42,6 +44,7 @@ interface DevBuild {
 export function devBuild({ buildFolder, imageCdnUrl, isProd } : DevBuild) {
     const VUE_CLI_PATH = path.join('.', 'node_modules', '@vue', 'cli-service', 'bin', 'vue-cli-service.js');
     const THEME_ENTRY_FILE = path.join('theme', 'index.js');
+    const isNodeVersionIsGreaterThan18 = +process.version.split(".")[0].slice(1) >= 18;
 
     return new Promise((resolve, reject) => {
         let b = exec(`node ${VUE_CLI_PATH} build --target lib --dest ${buildFolder} --name themeBundle ${THEME_ENTRY_FILE}`,
@@ -52,6 +55,7 @@ export function devBuild({ buildFolder, imageCdnUrl, isProd } : DevBuild) {
                     IMAGE_CDN_URL: imageCdnUrl,
                     NODE_ENV: (isProd && "production") || "development",
                     VUE_CLI_SERVICE_CONFIG_PATH: path.join(process.cwd(), Theme.VUE_CLI_CONFIG_PATH),
+                    ...(isNodeVersionIsGreaterThan18 && { NODE_OPTIONS:"--openssl-legacy-provider" })
                 }
             });
 
