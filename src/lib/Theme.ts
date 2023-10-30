11751ee3f0d7fa2e6b13eac830357fde1b8224db
Manish Prajapati <manishprajapati@gofynd.com>
2023-10-30 11:18:21 +0530

    ID: FPCO-23255; DONE: 10; HOURS: 1; Fix restructure issue where user needs to install dependencies again

--- src/lib/Theme.ts
+++ src/lib/Theme.ts
@@ -2762,7 +2762,7 @@ export default class Theme {
             fs.mkdirSync(destinationFolder, { recursive: true });
         }
         
-        const outer_items = ["package.json", "theme", "babel.config.js", "fdk.config.js", ".fdk", ".git", ".gitignore", ".husky"]
+        const outer_items = ["package.json", "package-lock.json", "debug.log", "assets.json", "pages.json", "theme", "babel.config.js", "fdk.config.js", ".fdk", ".git", ".gitignore", ".husky", "node_modules", "config.json"]
         const moved_files = []
         files.forEach((fileOrFolder) => {
             if (outer_items.includes(fileOrFolder)) return;
