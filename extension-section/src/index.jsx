d02e8d3f88a2b4df0c04b42c855c5c85a38bb1b2
Karan Raina <karanraina@gofynd.com>
2024-05-27 15:15:51 +0530

    fix-merge conflict

--- extension-section/src/index.jsx
+++ extension-section/src/index.jsx
@@ -1,5 +1,7 @@
 import * as ProductList from './sections/product-list';
+import * as TejasList from './sections/tejas-list';
 
 export default {
     'product-list': { ...ProductList, },
+    'demo-list': { ...TejasList, },
 }
