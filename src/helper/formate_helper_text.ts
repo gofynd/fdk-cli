f14322a95e79782b36211fff2ed95f76784e4217
jinalviranii <jinalvirani@gofynd.com>
2024-08-21 17:27:26 +0530

    Update commander helper text

--- src/helper/formate_helper_text.ts
+++ src/helper/formate_helper_text.ts
@@ -2,7 +2,7 @@ export function customHelpSectionData(entity){
     const temp = entity.helpInformation;
     entity.helpInformation = function (){
         let value = temp.call(this);
-        value = value.replace(/\|/g, ' | '); // Add space between command aliases
+        value = value.replace(/(Usage:\s.*?)(\|)(.*?\n)/, '$1 | $3'); // Add space between command aliases
         return value;
     }  
 }
