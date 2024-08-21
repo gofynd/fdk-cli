c28e5885e62925c90293cc465d7236532f2369a1
jinalviranii <jinalvirani@gofynd.com>
2024-08-21 17:27:26 +0530

    formate help data

--- src/commands/partner.ts
+++ src/commands/partner.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import Partner from '../lib/Partner';
+import { customHelpSectionData } from '../helper/formate_helper_text';
 
 export default function partner(program: Command) {
     const partner = new Command('partner').description('Partner Commands');
@@ -8,6 +9,6 @@ export default function partner(program: Command) {
         .command('connect')
         .description('Connect partner account')
         .asyncAction(Partner.connectHandler);
-
+    customHelpSectionData(partner)
     program.addCommand(partner);
 }
