97ba27f70ee02b37a88ac9ada4182d71adf183fc
Ajay Saini <ajaysaini@gofynd.com>
2025-07-29 08:50:06 +0530

    fix test cases

--- src/__tests__/globalTeardown.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  // Wait for any pending promises to resolve
-  await new Promise(resolve => setImmediate(resolve));
-  
-  // Force garbage collection if available
-  if (global.gc) {
-    global.gc();
-  }
-}; 
